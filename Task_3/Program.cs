b1f32264302bcea042fdf7a082191cdaf4c26558
tural10 <turalja@code.edu.az>
2023-03-19 11:09:14 +0400

    Добавьте файлы проекта.

---
+++ Task_3/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int say;
+        l1:
+            Console.Write("Telebelerin sayini qeyd edin: ");
+            if (!int.TryParse(Console.ReadLine(), out say) || say <= 0)
+            {
+                goto l1;
+            }
+            Student[] information = new Student[say];
+            for (int i = 0; i < information.Length; i++)
+            {
+                Student inform = new Student();
+
+                Console.Write($"{i + 1}.Telebenin adi: ");
+                inform.name = Console.ReadLine();
+                Console.Write($"{i + 1}.Telebenin soyadi: ");
+                inform.surname = Console.ReadLine();
+            l2:
+                Console.Write($"{i + 1}.Telebenin yashi: ");
+                if (!int.TryParse(Console.ReadLine(), out inform.age))
+                {
+                    goto l2;
+                }
+                Console.Write($"{i + 1}.Telebenin ixtisasi: ");
+                inform.speciality = Console.ReadLine();
+
+                Console.Write($"{i + 1}.Telebenin qrup nomresi: ");
+                inform.groupNo = Console.ReadLine();
+
+
+                information[i] = inform;
+                Console.WriteLine($"{information[i]} //");
+            }
+        }
+    }
+}
---
+++ Task_3/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    internal class Student
+    {
+        public string name;
+        public string surname;
+        public string speciality;
+        public string groupNo;
+        public int age;
+
+        public override string ToString()
+        {
+            return $"{name} {surname} {speciality} {groupNo} {age} ";
+        }
+    }
+}
